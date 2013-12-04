df758be5265b46417ff5122f78abf2e71cd40526
MellowPomelo <yonghuat@yahoo.com>
2013-12-04 22:19:01 +0900

    ch2

--- graphic_ch2.R
+++ graphic_ch2.R
@@ -27,3 +27,36 @@ qplot(Time, demand, data=BOD, geom="bar", stat="identity")
 ggplot(BOD, aes(x=Time, y=demand))+geom_bar(stat="identity")
 qplot(factor(cyl),data=mtcars)
 ggplot(mtcars, aes(x=factor(cyl)))+geom_bar()
+
+
+#histogram
+hist(mtcars$mpg)
+hist(mtcars$mpg, breaks=10)
+qplot(mtcars$mpg)
+qplot(mpg, data=mtcars, binwidth=4)
+ggplot(mtcars, aes(x=mpg))+geom_histogram(binwidth=4)
+
+#boxplot
+plot(ToothGrowth$supp, ToothGrowth$len)
+boxplot(len~supp, data=ToothGrowth)
+
+qplot(ToothGrowth$supp, ToothGrowth$len, geom="boxplot")
+qplot(supp, len, data=ToothGrowth, geom="boxplot")
+ggplot(ToothGrowth, aes(x=supp, y=len)) +geom_boxplot()
+
+boxplot(len~supp+dose, data=ToothGrowth)
+qplot(interaction(ToothGrowth$supp,ToothGrowth$dose), ToothGrowth$len, geom="boxplot")
+
+qplot(interaction(supp, dose), len, data=ToothGrowth, geom="boxplot")
+ggplot(ToothGrowth, aes(x=interaction(supp, dose), y=len))+geom_boxplot()
+
+#function curve
+curve(x^3-5*x, from=-4, to=4)
+myfun<-function(xvar){
+  1/(1+exp(-xvar+10))
+}
+curve(myfun(x), from=0, to=20)
+curve(1-myfun(x), add=T, col="red")
+
+qplot(c(0,20), fun=myfun, stat="function", geom="line")
+ggplot(data.frame(x=c(0,20)), aes(x=x))+stat_function(fun=myfun, geom="line")
